aef860b54649392f86db446c5d6f33624ea654e2
Tom van Dijk <tom@tvandijk.nl>
2015-01-24 10:49:25 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='janeus',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     url='http://github.com/jonge-democraten/janeus/',
     author='Jonge Democraten',
