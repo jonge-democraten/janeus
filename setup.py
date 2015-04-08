14d0a8ff7dedcef55835751720d3deb25d717334
Tom van Dijk <tom@tvandijk.nl>
2015-04-08 08:47:49 +0200

    Version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='janeus',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     url='http://github.com/jonge-democraten/janeus/',
     author='Jonge Democraten',
