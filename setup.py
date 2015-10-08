8b1aa244a9f3928a021a6a11da095471ce328d7c
Tom van Dijk <tom@tvandijk.nl>
2015-10-09 01:00:14 +0200

    Bump to version 1.4 with Mezzanine support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='janeus',
-    version='1.3',
+    version='1.4',
     packages=find_packages(),
     url='http://github.com/jonge-democraten/janeus/',
     author='Jonge Democraten',
